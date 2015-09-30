5eb5ad105e3a6a5bdb26838ea8d79e24d7cbb665
igordubovoy <idubovoy90@gmail.com>
2015-09-30 10:45:07 +0300

    photosite

---
+++ myproject/main/js/nav.js
@@ -0,0 +1,17 @@
+(function(){
+    var headerH = 128,
+        menu = document.querySelector('#navigation');
+        menu.style.cssText += " position: fixed; top: "+ headerH+"px";
+
+        window.addEventListener('scroll', scrollNav, false);
+
+        function scrollNav() {
+            var top = window.scrollY
+            console.log(top);
+            if(top < headerH) {
+            menu.style.cssText += "top: "+ (headerH-top)+"px";
+            }
+            else {  menu.style.cssText += "top:0";
+            }
+        }
+})()
