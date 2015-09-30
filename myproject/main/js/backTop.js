07b0c727f67b25cc7cc94896da496e6653e03266
igordubovoy <idubovoy90@gmail.com>
2015-09-30 18:11:47 +0300

    toTop

---
+++ myproject/main/js/backTop.js
@@ -0,0 +1,18 @@
+(function () {
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() != 0) {
+                $('#toTop').fadeIn();
+            } else {
+                $('#toTop').fadeOut();
+            }
+        });
+
+        $('#toTop').click(function () {
+
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1000);
+        });
+    });
+})()
