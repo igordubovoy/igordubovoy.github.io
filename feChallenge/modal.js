0ec20bd4ba538e2369ac3e9e13c62e7a993bbb6d
igordubovoy <idubovoy90@gmail.com>
2015-10-17 16:19:22 +0300

    with modal

---
+++ feChallenge/modal.js
@@ -0,0 +1,28 @@
+(function () {
+    var dark = document.getElementById('dark'),
+        modal = document.querySelector('.modal-form');
+
+
+    if (window.addEventListener) {
+        window.addEventListener('scroll', viewModal, false);
+    } else {
+        window.attachEvent('scroll', viewModal);
+    }
+
+    var style = window.getComputedStyle(document.body),
+        prop = +style.height.substring(0, style.height.length - 2);
+
+
+
+    function viewModal() {
+        var scroll = window.scrollY * 100 / prop;
+        console.log(scroll)
+        if (scroll > 68.6) {
+            dark.className = "dark_monitor";
+            modal.style.cssText += "visibility: visible; z-index: 1000";
+        } else {
+            dark.className = "";
+            modal.style.cssText = "";
+        }
+    }
+})()
