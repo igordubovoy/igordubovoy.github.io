775841fd44549e0a1c93bac3af551022ebc58c79
igordubovoy <idubovoy90@gmail.com>
2015-10-23 22:43:57 +0300

    feChallenge2.0

--- feChallenge/modal.js
+++ feChallenge/modal.js
@@ -1,4 +1,4 @@
-(function () {
+(function(){
     var dark = document.getElementById('dark'),
         modal = document.querySelector('.modal-form');
 
@@ -9,16 +9,12 @@
         window.attachEvent('scroll', viewModal);
     }
 
-    var style = window.getComputedStyle(document.body),
-        prop = +style.height.substring(0, style.height.length - 2);
-    console.log(prop)
-
-
     function viewModal() {
-        var scroll = window.scrollY * 100 / prop;
+        var style = window.getComputedStyle(document.body),
+        prop = +style.height.substring(0, style.height.length - 2),
+        scroll = window.scrollY * 100 / prop;
         console.log(scroll)
-
-        if (scroll > 99) {
+        if (scroll > 74.16) {
             dark.className = "dark_monitor";
             modal.style.cssText += "visibility: visible; z-index: 1000";
         } else {
